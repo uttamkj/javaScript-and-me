c5680203391c224817969a0e625e24d8d47cca22
Uttam Kumar Jena <145798902+uttamkj@users.noreply.github.com>
2025-01-27 00:17:27 +0530

    Datatypes

---
+++ A02.js
@@ -0,0 +1,45 @@
+"use strict" ;
+
+ // treat all js code as newer virsion 
+// alert(3+3)  // we are using nodejs , not browser
+
+// ECMA Script  
+
+
+let name = "uttam"
+let age = 23
+let isLogin = true
+// null and undefined datatypes
+let x = null
+let y;
+//symbol datatype used in react to check unique ness
+// object 
+// console.log(typeof(5));
+// console.log(typeof(null));  // object 
+
+let scroe = 85
+let mark = "99"
+let mark1 = "99abc"
+let mark2 = null
+let u ;
+let num = Number(mark)
+let num1 = Number(mark1)
+let num2 = Number(mark2)
+let num3 = Number(u)
+let t = Number(true)
+let f = Number(false)
+// console.table([num,num1,num2,num3,t,f])
+
+
+let a = Boolean(1)
+
+// 1 => true 0 => false 
+// "" => false "bahs" = > true 
+
+let number = 1233
+
+let strNum = String(number)
+
+console.log(2+2); //4
+console.log(2+2+"2"); //42
+console.log("2"+2+5);  // 225
