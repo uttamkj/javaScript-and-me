1415ba70560e056008da18e7722dce8bf90795a9
Uttam Kumar Jena <145798902+uttamkj@users.noreply.github.com>
2025-01-27 20:43:08 +0530

    part 3

---
+++ A03.js
@@ -0,0 +1,21 @@
+const id = Symbol("123")
+const newId = Symbol("123")
+
+// console.log(id === newId);
+
+const bigIntV = 12456789551n
+
+//Strings
+
+let name = "Uttam"
+let repoCount = 50
+
+console.log(`Hello my name is ${name} and my total repo in github is ${repoCount}`)
+
+const gameName = new String('freeFire')
+console.log(gameName);
+
+const score = new Number(100)
+console.log(score)
+
+console.log(`He scored ${score} in the game ${gameName}`)
